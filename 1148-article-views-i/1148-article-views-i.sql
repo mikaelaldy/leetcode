7f61d1238e50e0578e85b29ea20018143be229dc
Mikael A <58765940+mikaelaldy@users.noreply.github.com>
2024-02-04 13:32:20 +0700

    Time: 756 ms (57.55%), Space: 0B (100.00%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -3,3 +3,5 @@ SELECT DISTINCT author_id as id
 FROM Views
 where author_id = viewer_id
 order by id ASC;
+
+# gunakan select distinct untuk memilih author_id yang memiliki nilai unik ( tidak duplikat)
